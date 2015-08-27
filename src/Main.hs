7d0aa1178f7cc00951e1d752cdb59195abcb3b54
Hong Minhee <hongminhee@member.fsf.org>
2015-08-28 03:44:23 +0900

    CLI

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,35 @@
-import Network.Wai.Handler.Warp (run)
+{-# LANGUAGE DeriveDataTypeable #-}
+import Data.String (fromString)
+
+import Network.Wai.Handler.Warp ( HostPreference, Port
+                                , defaultSettings, runSettings
+                                , setHost, setPort, setTimeout
+                                )
+import System.Console.CmdArgs ( Data, Typeable
+                              , cmdArgs, help, opt, summary, typ, (&=)
+                              )
 
 import Web.Feed.FeedTranslatorWai (application)
 
-port :: Int
-port = 8080
+data FeedTranslator = FeedTranslator { host :: String
+                                     , port :: Port
+                                     , timeout :: Int
+                                     } deriving (Show, Data, Typeable)
 
 main :: IO ()
 main = do
+    args <- cmdArgs $
+        FeedTranslator { host = "0.0.0.0" &= typ "HOST"
+                                          &= help "host to listen [0.0.0.0]"
+                       , port = 8080 &= typ "PORT"
+                                     &= help "port to listen [8080]"
+                       , timeout = 30 &= typ "SECONDS"
+                                      &= help "timeout in seconds [30]"
+                       } &= summary "Translate syndication feeds"
     app <- application
-    putStrLn ("Running on http://0.0.0.0:" ++ show port ++
+    let settings = setHost (fromString $ host args) $
+                   setPort (port args) $
+                   setTimeout (timeout args) defaultSettings
+    putStrLn ("Running on http://" ++ host args ++ ":" ++ show (port args) ++
               "/ (Press CTRL+C to quit)")
-    run port app
+    runSettings settings app
